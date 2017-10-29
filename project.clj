6ceb686ffc3f60a53a239d358a2fff88ad5175d4
Michael D Pendergrass <mdpendergrass@gmail.com>
2017-10-29 14:08:08 -0400

    update dependencies and remove redundant dependency

--- project.clj
+++ project.clj
@@ -9,20 +9,31 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[ring "1.4.0"]
-                 [slingshot "0.12.2"]
-                 [org.pupcus/stateful-sessions "0.0.2"]]
+  :dependencies [[slingshot "0.12.2"]
+                 [org.pupcus/stateful-sessions "0.0.3"]]
 
   :profiles {:dev  {:resource-paths ["dev-resources"]
                     :dependencies [[org.clojure/clojure "1.8.0"]
-                                   [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
+                                   [org.slf4j/slf4j-log4j12 "1.7.25"]]}}
 
   :deploy-repositories [["snapshots"
                          {:url "https://clojars.org/repo"
+                          :sign-releases false
                           :creds :gpg}]
                         ["releases"
                          {:url "https://clojars.org/repo"
+                          :sign-releases false
                           :creds :gpg}]]
+
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
 
   :global-vars {*warn-on-reflection* true
                 *assert* false})
